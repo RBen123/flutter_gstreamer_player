1d8398d70e412f3ce2c6e291316364f70f712c79
Robert Bergmann <robert.bergmann@rosenbauer.com>
2024-10-14 11:02:39 +0200

    update gstreamer player

--- gst_player/gst_player.cc
+++ gst_player/gst_player.cc
@@ -57,11 +57,11 @@ void GstPlayer::play(const gchar* pipelineString, const gchar* rtmpString) {
 
     // Set the pipeline to PLAYING state and wait for the transition
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    GstStateChangeReturn ret = gst_element_get_state(pipeline, nullptr, nullptr, GST_CLOCK_TIME_NONE);
-    if (ret != GST_STATE_CHANGE_SUCCESS) {
-        g_printerr("Failed to set pipeline to PLAYING state.");
-        freeGst();
-    }
+//    GstStateChangeReturn ret = gst_element_get_state(pipeline, nullptr, nullptr, GST_CLOCK_TIME_NONE);
+//    if (ret != GST_STATE_CHANGE_SUCCESS) {
+//        g_printerr("Failed to set pipeline to PLAYING state.");
+//        freeGst();
+//    }
 }
 
 
