38f96ce46c5e0427baf279a71ccee3ec490cf2c9
Robert Bergmann <robert.bergmann@rosenbauer.com>
2024-10-14 14:10:07 +0200

    update

--- lib/flutter_gstreamer_player.dart
+++ lib/flutter_gstreamer_player.dart
@@ -13,7 +13,7 @@ class GstPlayerTextureController {
   Future<int> initialize(String pipeline, String rtmp) async {
     // No idea why, but you have to increase `_id` first before pass it to method channel,
     // if not, receiver of method channel always received 0
-    GstPlayerTextureController._id = GstPlayerTextureController._id + 1;
+    GstPlayerTextureController._id = 1;//GstPlayerTextureController._id + 1;
 
     textureId = await _channel.invokeMethod('PlayerRegisterTexture', {
       'pipeline': pipeline,
